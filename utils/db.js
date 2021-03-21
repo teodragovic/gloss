f0f934525b23fd79a505493db19c34da4baf860f
Teo Dragovic <teo.dragovic@gmail.com>
2021-03-21 10:38:16 +0100

    wip: add utils

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,23 @@
-import faunadb from 'faunadb';
 
-const client = new faunadb.Client({
+import faunadb from 'faunadb';
+import cookie from 'cookie';
+
+export const FAUNA_SECRET_COOKIE = 'faunaSecret';
+
+export const serverClient = new faunadb.Client({
     secret: process.env.FAUNA_SERVER_KEY
 });
 
-const q = faunadb.query;
+// Used for any authed requests.
+export const faunaClient = (secret) => new faunadb.Client({ secret });
 
-export { client, q };
+export const serializeFaunaCookie = (userSecret) => {
+    const cookieSerialized = cookie.serialize(FAUNA_SECRET_COOKIE, userSecret, {
+        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 72576000,
+        httpOnly: true,
+        path: '/',
+    });
+    return cookieSerialized;
+};

0f6af8a4c0115e17af282ea4058d7f5d06751130
Teo Dragovic <teo.dragovic@gmail.com>
2021-05-02 11:58:28 +0200

    Add material ui

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,20 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/main.scss';
 
+import { Fragment } from 'preact';
+import Head from 'next/head';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 function MyApp({ Component, pageProps }) {
-    return <Component { ...pageProps } />;
+    return (
+        <Fragment>
+            <CssBaseline />
+            <Head>
+                <title>Gloss</title>
+            </Head>
+            <Component { ...pageProps } />
+        </Fragment>
+    );
 }
 
 export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+
+// https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                        media="print"
+                        onLoad="this.media='all'"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
